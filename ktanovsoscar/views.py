62c9e0b15515833a4768f9e9889d0cf819afb861
EllisBell <tee.vee.zed@gmail.com>
2015-02-08 19:07:15 +0000

    more setup

--- ktanovsoscar/views.py
+++ ktanovsoscar/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("this is going to be an oscar predictions website </br> stay tuned...")
+	return render(request, 'ktanovsoscar/index.html')
